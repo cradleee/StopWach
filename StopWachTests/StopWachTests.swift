586e7fb344b723a42f7f6b8315b8f335583006a1
釜野由里佳 <yrkmn19@gmail.com>
2018-02-07 17:43:54 +0900

    Initial Commit

---
+++ StopWachTests/StopWachTests.swift
@@ -0,0 +1,36 @@
+//
+//  StopWachTests.swift
+//  StopWachTests
+//
+//  Created by kamano yurika on 2018/02/07.
+//  Copyright © 2018年 釜野由里佳. All rights reserved.
+//
+
+import XCTest
+@testable import StopWach
+
+class StopWachTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
