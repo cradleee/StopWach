17ac0418376f7dad844e03425de9fac85317c469
釜野由里佳 <yrkmn19@gmail.com>
2018-02-08 12:15:08 +0900

    エラーあり

--- StopWach/ViewController.swift
+++ StopWach/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
+    var count: Float = 0.0
+    
+    var timer: Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @objc func up () {
+        //
+        count = count + 0.01
+        //
+        label.text = String(format: "%.2f",count)
+    }
+    
+    @IBAction func start() {
+        if timer.isValid{
+           //
+          timer.invalidate()
+          //
+          self.hantei()
+        }
+    }
+        
+        if !timer.isValid {
+            //
+            
+            timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                         target: self,
+                                         selector: #selector(self.up),
+                                         userInfo: nil,
+                                         repeats: true
+            )
+        }
+    }
+    
+    @IBAction func stop() {
+        if timer.isValid {
+            //
+            timer.invalidate()
+        }
+    }
 }
 
